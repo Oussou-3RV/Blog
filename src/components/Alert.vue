2984e9009ccb72e890698394c9a00413eb83a040
OUSMANE BALDE <baldemarc224@gmail.com>
2025-05-31 01:03:51 -0400

    Implementation d'une alert au cas ou mise à jour impossible

---
+++ src/components/Alert.vue
@@ -0,0 +1,43 @@
+<template>
+    <div :class="classes">
+        <slot></slot>
+        <button @click="Emits('close')">&times;</button>
+    </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    type:String
+})
+
+const Emits = defineEmits('close')
+const classes = computed(()=> `alert alert-${props.type}`)
+</script>
+
+<style>
+.alert{
+    position: relative;
+    padding: 0.75rem 1.25rem;
+    margin-bottom: 1rem;
+    border: 1px solid red;
+    border-radius: .25rem;
+}
+
+.alert-danger{
+    color: #721c24;
+    background-color: #f8d7da;
+    border-color: #f5c6cb;
+
+}
+
+.alert button{
+    background-color: transparent;
+    border: none;
+    position: absolute;
+    right:.50rem;
+    top: 0rem;
+}
+
+</style>
